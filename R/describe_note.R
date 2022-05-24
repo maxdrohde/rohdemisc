e9283029f1de2c787a17ed633dc8ef1f2c7b0edd
Max Rohde <maxdrohde@gmail.com>
2022-05-24 13:31:47 -0500

    new

---
+++ R/describe_note.R
@@ -0,0 +1,20 @@
+#' Return `Hmisc::describe` in a Quarto callout note
+#'
+#'
+#' Note: You must set `#| output = asis` within the Quarto block
+#' @param data A data frame as input to `Hmisc::describe()`.
+#' @param label A label for the note heading
+#'
+#' @export
+#'
+#' @examples
+#' describe_note(mtcars)
+#' describe_note(mtcars, label = "Data values in the `mtcars` dataset")
+describe_note <- function(data,
+                          label = "Expand for an overview of data values"){
+  cat(":::{.callout-note collapse='true'}\n")
+  cat(paste("##", label))
+  cat("\n")
+  cat(Hmisc::html(Hmisc::describe(data)))
+  cat("\n:::")
+}
